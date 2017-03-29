5a8188457d30416eca00f6e19ab7100ccec1d425
RedDread <jpg.almeida277@gmail.com>
2017-03-29 19:29:39 +0100

    CHANGES

--- Calculator_New/Calculator_New/Form1.Designer.cs
+++ Calculator_New/Calculator_New/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBoxDisplay = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -68,26 +69,39 @@
             // 
             // textBoxDisplay
             // 
+            this.textBoxDisplay.Enabled = false;
             this.textBoxDisplay.Location = new System.Drawing.Point(12, 36);
             this.textBoxDisplay.Multiline = true;
             this.textBoxDisplay.Name = "textBoxDisplay";
             this.textBoxDisplay.Size = new System.Drawing.Size(267, 65);
             this.textBoxDisplay.TabIndex = 1;
+            this.textBoxDisplay.Text = "0";
+            this.textBoxDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 107);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 37);
+            this.button1.Size = new System.Drawing.Size(40, 40);
             this.button1.TabIndex = 2;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(291, 278);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxDisplay);
             this.Controls.Add(this.menuStrip1);
@@ -110,6 +124,7 @@
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.TextBox textBoxDisplay;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
